41d16e45ff53fe0da8485d65b76931a65643acd2
Boralov Serg <89090168690@mail.ru>
2023-11-07 16:03:26 +0500

    update

--- web/app.py
+++ web/app.py
@@ -1,4 +1,18 @@
+import os
+import mysql.connector as mariadb
 from flask import Flask
+
+
+
+
+connection = mariadb.connect(
+    user=os.environ.get("DB_USERNAME"),
+    password=os.environ.get("DB_PASSWORD"),
+    database=os.environ.get("DB"),
+    host=os.environ.get("HOST"),
+    port=os.environ.get("PORT"))
+
+
 
 app = Flask(__name__)
 
@@ -7,6 +21,22 @@ app = Flask(__name__)
 def status_ok():
     return "<h1>Hello, Flask</h1>"
 
+
+@app.route('/health')
+def health():
+    return {"status": "OK"}
+
+@app.route('/db')
+def db_ok():
+    json_data = []
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    # result = cursor.fetchall()
+    # connection.close()
+    row_headers=[x[0] for x in cursor.description] 
+    for result in cursor:
+        json_data.append(dict(zip(row_headers,result)))
+    return json_data
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
